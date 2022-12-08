f6d8a20ff09fa189b28ae58b4c623f684897b260
Navneet <3172001nav@gmail.com>
2022-12-08 12:16:05 +0530

    completed the code for first server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const http = require("http");
+const http = require('http');
 
-const port = 8080;
+const port =8082;
 
-http
-.createServer((require , response) => {
-    response.writeHead(200 , {"content-type": "text/html"});
-    response.write("<h1>Hello, this is from my first server</h1>");
+http.createServer((require, response) => {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write("<h1>Hello, this is my first server</h1>");
     response.end();
-})
 
-.listen(port, () =>  {
-    console.log('Nodejs server started on port ${port}');
+})
+.listen(port, () => {
+    console.log('Node.js server started on port ${port}');
 });
